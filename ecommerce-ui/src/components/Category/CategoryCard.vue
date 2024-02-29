d432c868a80a60e3467b4a220771379b6ed66274
Jovan Bogdanovic <bogdanovicjob@gmail.com>
2024-02-29 17:15:42 +0100

    Initial commit

---
+++ ecommerce-ui/src/components/Category/CategoryCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card ">
+        <div>
+            <img :src="category.imageUrl" class="card-img-top" alt="...">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ category.categoryName }}</h5>
+            <p class="card-text">{{ category.description }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: CategoryCard,
+    props: ["category"],
+    methods: {
+
+    }
+}
+</script>
---
+++ ecommerce-ui/src/views/Category/CategoryList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3 class="pt-3">Categories</h3>
+            </div>
+            <div>{{ categories[0] }}</div>
+        </div>
+        <div class="row">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+const axios = require("axios");
+//import categoryCard from '../../components/Category/CategoryCard.vue';
+export default{
+    data(){
+        return {
+            baseUrl: "https://limitless-lake-55070.herokuapp.com",
+            categories: []
+        };
+    },
+    methods: {
+        async getCategories(){
+            await axios.get(`${this.baseUrl}/category/`)
+            .then(res => this.categories = res.data)
+            .catch(err => console.log(err))
+        }
+    },
+    mounted(){
+        this.getCategories();
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
